2bb024379c3a8eadf54760113d7241662860b5be
Paul Cody <pcj@pubref.org>
2016-05-15 10:42:12 -0600

    Semicolon fixes

--- src/node_etal.js
+++ src/node_etal.js
@@ -8,7 +8,7 @@ CSL.Node["et-al"] = {
                 if ("string" === typeof this.strings.term) {
                     state.tmp.etal_term = this.strings.term;
                 }
-            }
+            };
             this.execs.push(func);
         }
         target.push(this);
